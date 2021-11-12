67594f85e980f58275be1f25e25ed605f41a9e5d
DulceM20 <dulce_mirandaa_1@hotmail.com>
2021-11-11 22:31:47 -0600

    what Imade in day 1

---
+++ day1/e1.py
@@ -0,0 +1,21 @@
+#int
+a=2
+#float
+b=2.3
+#string
+c="hello world"
+#Boolean
+d=True
+e=False
+
+print(c)
+
+x=1
+x=x+1
+print(x)
+
+#Type data
+city= (input("Type the citywhere you grew up: "))
+print(type(city))
+
+num1=int(input("Type"))
---
+++ day1/e2.py
@@ -0,0 +1,4 @@
+print("Welcom to the band name generator")
+city = input ("What is the name of the city you grew up in? ")
+pet= input ("What is your pet's name? ")
+print(f"Your band name could be {city} {pet}")
